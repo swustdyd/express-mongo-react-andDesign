5981a315ae874b138dd03f8aa81a5c8a9aa24f79
aarondeng <1562044678@qq.com>
2018-03-16 16:56:31 +0800

    加入了显示文件打包情况的工具

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 let webpack = require('webpack');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 let path = require('path');
 let baseConfig = require('./baseConfig');
 let devPort = process.env.PORT || baseConfig.devPort;
@@ -10,7 +11,7 @@ let hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
 let devConfig = {
     entry: {
         index: ['./client/app', hotMiddlewareScript],
-        vendor: ['react','react-dom','react-router-dom']
+        vendor: ['react','react-dom','react-router-dom', 'redux', 'react-redux', 'redux-thunk', 'redux-logger']
     },
     output: {
         filename: './js/[name].bundle.js',
@@ -66,7 +67,35 @@ let devConfig = {
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
+            minChunks: Infinity,
             filename: './js/[name].bundle.js'
+        }),
+        new BundleAnalyzerPlugin({
+            // Can be `server`, `static` or `disabled`.
+            // In `server` mode analyzer will start HTTP server to show bundle report.
+            // In `static` mode single HTML file with bundle report will be generated.
+            // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+            analyzerMode: 'server',
+            // Host that will be used in `server` mode to start HTTP server.
+            analyzerHost: '127.0.0.1',
+            // Port that will be used in `server` mode to start HTTP server.
+            analyzerPort: 8888,
+            // Path to bundle report file that will be generated in `static` mode.
+            // Relative to bundles output directory.
+            reportFilename: 'report.html',
+            // Automatically open report in default browser
+            openAnalyzer: true,
+            // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+            generateStatsFile: false,
+            // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+            // Relative to bundles output directory.
+            statsFilename: 'stats.json',
+            // Options for `stats.toJson()` method.
+            // For example you can exclude sources of your modules from stats file with `source: false` option.
+            // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+            statsOptions: null,
+            // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+            logLevel: 'info'
         })
     ]
 };
