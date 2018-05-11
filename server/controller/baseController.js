0406e1f74015ec2529ac8562117d251884953274
aarondeng <1562044678@qq.com>
2018-05-11 10:02:36 +0800

    修改了eslintignore配置，去除不必要的文件忽略

--- server/controller/baseController.js
+++ server/controller/baseController.js
@@ -1,8 +1,8 @@
 /*
  * @Author: yedong.deng 
  * @Date: 2018-05-10 17:24:07 
- * @Last Modified by:   aaron.deng 
- * @Last Modified time: 2018-05-10 17:24:07 
+ * @Last Modified by: yedong.deng
+ * @Last Modified time: 2018-05-11 09:29:02
  */
 export default class BaseController{
     
