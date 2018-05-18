d90e530b4461052b27ebc0d5aa510a597524137f
swustdyd <1562044678@qq.com>
2018-05-18 22:02:56 +0800

    添加了userAgent，以及做了某些修改

--- server/spider/doubanMovieType.js
+++ server/spider/doubanMovieType.js
@@ -6,58 +6,24 @@ import BaseConfig from '../../baseConfig'
 mongoose.connect(BaseConfig.dbConnectString);
 const doubanMovieService = new DoubanMovieService();
 
+doUpdate();
+
 async function doUpdate() {
     try {
-        const typeKeys = ['types', 'countries', 'languages'];
         const {total} = await doubanMovieService.getDoubanMovies();
-        const pageSize = 20;
+        const pageSize = 50;
         const pageEndIndex = Math.ceil(total / pageSize);
         console.log(`电影总数为：${total}, pageEndIndex: ${pageEndIndex}, pageSize: ${pageSize}`);
         for(let pageIndex = 0; pageIndex < pageEndIndex; pageIndex++){
             console.log(`更新数据：${pageIndex * pageSize + 1} 到 ${pageIndex * pageSize + pageSize} 条`);
-            const {result} = await doubanMovieService.getDoubanMovies(pageIndex, pageSize);
+            const {result} = await doubanMovieService.getDoubanMovies(pageIndex, pageSize);            
+            const promiseList = [];
             for(let i = 0; i < result.length; i++){
                 const doubanMovie = result[i];
-                for(let j = 0; j < typeKeys.length; j++){
-                    const typeKey = typeKeys[j];
-                    const movieProperty = doubanMovie[typeKey];
-                    if(movieProperty instanceof Array){
-                        for(let k = 0; k < movieProperty.length; k++){
-                            const {result: movieTypes} = await doubanMovieService.getMovieTypeByOption({
-                                condition: {
-                                    mongoObjectId: doubanMovie._id,
-                                    typeKey: typeKey,
-                                    typeValue: movieProperty[k]
-                                }
-                            });
-                            if(movieTypes.length < 1){
-                                await doubanMovieService.saveMovieAndType({
-                                    doubanMovieId: doubanMovie.doubanMovieId,
-                                    mongoObjectId: doubanMovie._id,
-                                    typeKey: typeKey,
-                                    typeValue: movieProperty[k]
-                                })
-                            }
-                        }
-                    }else{
-                        const {result: movieTypes} = await doubanMovieService.getMovieTypeByOption({
-                            condition: {
-                                mongoObjectId: doubanMovie._id,
-                                typeKey: typeKey,
-                                typeValue: movieProperty
-                            }
-                        });
-                        if(movieTypes.length < 1){
-                            await doubanMovieService.saveMovieAndType({
-                                doubanMovieId: doubanMovie.doubanMovieId,
-                                mongoObjectId: doubanMovie._id,
-                                typeKey: typeKey,
-                                typeValue: movieProperty
-                            })
-                        }
-                    }
-                }
+                promiseList.push(updateOne(doubanMovie));
             }
+            await Promise.all(promiseList);
+
         }
         console.log('更新成功');
         process.exit();
@@ -68,4 +34,45 @@ async function doUpdate() {
     }
 }
 
-doUpdate();
+async function updateOne(doubanMovie){
+    const typeKeys = ['types', 'countries', 'languages'];
+    for(let j = 0; j < typeKeys.length; j++){
+        const typeKey = typeKeys[j];
+        const movieProperty = doubanMovie[typeKey];
+        if(movieProperty instanceof Array){
+            for(let k = 0; k < movieProperty.length; k++){
+                const {result: movieTypes} = await doubanMovieService.getMovieTypeByOption({
+                    condition: {
+                        mongoObjectId: doubanMovie._id,
+                        typeKey: typeKey,
+                        typeValue: movieProperty[k]
+                    }
+                });
+                if(movieTypes.length < 1){
+                    await doubanMovieService.saveMovieAndType({
+                        doubanMovieId: doubanMovie.doubanMovieId,
+                        mongoObjectId: doubanMovie._id,
+                        typeKey: typeKey,
+                        typeValue: movieProperty[k]
+                    })
+                }
+            }
+        }else{
+            const {result: movieTypes} = await doubanMovieService.getMovieTypeByOption({
+                condition: {
+                    mongoObjectId: doubanMovie._id,
+                    typeKey: typeKey,
+                    typeValue: movieProperty
+                }
+            });
+            if(movieTypes.length < 1){
+                await doubanMovieService.saveMovieAndType({
+                    doubanMovieId: doubanMovie.doubanMovieId,
+                    mongoObjectId: doubanMovie._id,
+                    typeKey: typeKey,
+                    typeValue: movieProperty
+                })
+            }
+        }
+    }
+}
--- server/spider/doubanSpider.js
+++ server/spider/doubanSpider.js
@@ -31,7 +31,18 @@ const doubanCookies = [
     'll="118318"; _vwo_uuid_v2=268A153A3B0C643D147E31481E0895A1|0a1395ad6b587e425cf1f4bd99fad90b; bid=fkXI8qz-GAc; ct=y; _ga=GA1.2.1991939520.1484715813; _gid=GA1.2.25358410.1526450489; push_noty_num=0; push_doumail_num=0; __utmc=30149280; ps=y; __utmc=223695111; __utmv=30149280.17867; __utma=30149280.1991939520.1484715813.1526536367.1526542648.5; __utmz=30149280.1526542648.5.5.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/safety/unlock_sms/resetpassword; __utmt=1; dbcl2="178675845:SCWwIdusqZQ"; ck=TDwN; __utmb=30149280.3.10.1526542648; __utma=223695111.782172476.1501562695.1526536459.1526542669.14; __utmb=223695111.0.10.1526542669; __utmz=223695111.1526542669.14.9.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526542669%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_ses.100001.4cf6=*; _pk_id.100001.4cf6=2b9398a388e83bef.1501562694.14.1526542823.1526536472.',
     'll="118318"; _vwo_uuid_v2=268A153A3B0C643D147E31481E0895A1|0a1395ad6b587e425cf1f4bd99fad90b; bid=fkXI8qz-GAc; ct=y; _ga=GA1.2.1991939520.1484715813; _gid=GA1.2.25358410.1526450489; push_noty_num=0; push_doumail_num=0; __utmc=30149280; ps=y; __utmc=223695111; __utmv=30149280.17867; __utma=30149280.1991939520.1484715813.1526542648.1526549136.6; __utmz=30149280.1526549136.6.6.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmt=1; _gat_UA-7019765-1=1; dbcl2="178675845:A9HrDkfWGLM"; ck=JcDw; __utmb=30149280.4.10.1526549136; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526549246%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_ses.100001.4cf6=*; __utma=223695111.782172476.1501562695.1526542669.1526549246.15; __utmb=223695111.0.10.1526549246; __utmz=223695111.1526549246.15.10.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _pk_id.100001.4cf6=2b9398a388e83bef.1501562694.15.1526549260.1526542823.',
     'll="118318"; bid=vjsHjHlnwLk; __yadk_uid=KcwFj36IDYtfMCCcbIvW0WvBmECi2OUp; ps=y; _vwo_uuid_v2=D13A1E2CA9F099C5F03963125D6884832|48861d9c1aa1c9357e7778210a464d33; __utmt=1; dbcl2="178675845:boYlLr//cHw"; ck=O-Qz; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526564577%2C%22https%3A%2F%2Faccounts.douban.com%2Flogin%3Falias%3D18381669933%26redir%3Dhttps%253A%252F%252Fmovie.douban.com%252F%26source%3DNone%26error%3D1013%22%5D; _pk_id.100001.4cf6=6d19214551336a9b.1526515966.2.1526564585.1526515969.; _pk_ses.100001.4cf6=*; __utma=30149280.844991547.1526515961.1526515961.1526564502.2; __utmb=30149280.2.10.1526564502; __utmc=30149280; __utmz=30149280.1526564502.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.921000792.1526515966.1526515966.1526564577.2; __utmb=223695111.0.10.1526564577; __utmc=223695111; __utmz=223695111.1526564577.2.2.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/login; push_noty_num=0; push_doumail_num=0',
-    'll="118318"; _vwo_uuid_v2=268A153A3B0C643D147E31481E0895A1|0a1395ad6b587e425cf1f4bd99fad90b; bid=fkXI8qz-GAc; ct=y; _ga=GA1.2.1991939520.1484715813; _gid=GA1.2.25358410.1526450489; push_noty_num=0; push_doumail_num=0; ps=y; __utmv=30149280.17867; __utmc=30149280; __utmc=223695111; __yadk_uid=N5x9XuFvFGEmEDljyt2GTxdl6UcQv5fI; __utma=30149280.1991939520.1484715813.1526628853.1526630817.10; __utmz=30149280.1526630817.10.9.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/safety/unlock_sms/resetpassword; __utmt=1; __utmb=30149280.1.10.1526630817; dbcl2="178675845:GHJzo6raewc"; ck=OGYO; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526630832%2C%22https%3A%2F%2Fwww.douban.com%2Faccounts%2Flogin%3Fredir%3Dhttps%253A%252F%252Fmovie.douban.com%252F%22%5D; _pk_ses.100001.4cf6=*; __utma=223695111.782172476.1501562695.1526625707.1526630832.18; __utmb=223695111.0.10.1526630832; __utmz=223695111.1526630832.18.13.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/accounts/login; _pk_id.100001.4cf6=2b9398a388e83bef.1501562694.18.1526630838.1526626641.'
+    'll="118318"; _vwo_uuid_v2=268A153A3B0C643D147E31481E0895A1|0a1395ad6b587e425cf1f4bd99fad90b; bid=fkXI8qz-GAc; ct=y; _ga=GA1.2.1991939520.1484715813; _gid=GA1.2.25358410.1526450489; push_noty_num=0; push_doumail_num=0; ps=y; __utmv=30149280.17867; __utmc=30149280; __utmc=223695111; __yadk_uid=N5x9XuFvFGEmEDljyt2GTxdl6UcQv5fI; __utma=30149280.1991939520.1484715813.1526628853.1526630817.10; __utmz=30149280.1526630817.10.9.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/safety/unlock_sms/resetpassword; __utmt=1; __utmb=30149280.1.10.1526630817; dbcl2="178675845:GHJzo6raewc"; ck=OGYO; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526630832%2C%22https%3A%2F%2Fwww.douban.com%2Faccounts%2Flogin%3Fredir%3Dhttps%253A%252F%252Fmovie.douban.com%252F%22%5D; _pk_ses.100001.4cf6=*; __utma=223695111.782172476.1501562695.1526625707.1526630832.18; __utmb=223695111.0.10.1526630832; __utmz=223695111.1526630832.18.13.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/accounts/login; _pk_id.100001.4cf6=2b9398a388e83bef.1501562694.18.1526630838.1526626641.',
+    'll="118318"; bid=vjsHjHlnwLk; __yadk_uid=KcwFj36IDYtfMCCcbIvW0WvBmECi2OUp; ps=y; __utmt=1; dbcl2="178675845:8b8wWwTCQOQ"; ck=LDEV; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1526649626%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _vwo_uuid_v2=D13A1E2CA9F099C5F03963125D6884832|48861d9c1aa1c9357e7778210a464d33; _pk_id.100001.4cf6=6d19214551336a9b.1526515966.3.1526649629.1526564585.; _pk_ses.100001.4cf6=*; __utma=30149280.844991547.1526515961.1526644071.1526649613.4; __utmb=30149280.3.10.1526649613; __utmc=30149280; __utmz=30149280.1526649613.4.4.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/safety/unlock_sms/resetpassword; __utmv=30149280.17867; __utma=223695111.921000792.1526515966.1526564577.1526649626.3; __utmb=223695111.0.10.1526649626; __utmc=223695111; __utmz=223695111.1526649626.3.3.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; push_noty_num=0; push_doumail_num=0'
+]
+
+const userAgents = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60',
+    'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2 ',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36'
 ]
 
 /**
@@ -62,12 +73,12 @@ const sorts = ['recommend', 'time', 'rank'];
 /**
  * 随机headers
  */
-let radomHeaders = getRadomHeaders();
+let radomHeader = getRadomHeaders();
 
 /**
  * 请求延时：毫秒
  */
-const delayTime = 1000;
+const delayTime = 2000;
 
 /**
  * 开始从豆瓣网获取movie
@@ -120,7 +131,7 @@ async function parseAndSaveMovieWithTag(doubanMovieId: string, doubanMovieTitle:
         const resData = await HttpsUtil.getAsync({
             hostname: 'movie.douban.com',
             path: `/subject/${doubanMovieId}/?from=showing`,
-            headers: radomHeaders
+            headers: radomHeader
         }, 'utf-8');
         
         //延时，免得被豆瓣封ip
@@ -135,6 +146,10 @@ async function parseAndSaveMovieWithTag(doubanMovieId: string, doubanMovieTitle:
             const saveResult = await doubanMovieService.saveOrUpdateDoubanMovie(doubanMovie);
             parseSuccessCount++;
             console.log(`第${parseSuccessCount}部电影 “${saveResult.doubanMovieId}——${saveResult.name}” 解析成功`);
+            //20次请求换一次header
+            if(httpsSuccessCount % 20 === 0){
+                radomHeader = getRadomHeaders();
+            }
         }else{
             console.log(`/***** 第${parseIndex}部电影 “${doubanMovieId}——${doubanMovieTitle}” 解析失败, statusCode：${statusCode} *****/`);
             logger.error(`第${parseIndex}部电影 “${doubanMovieId}——${doubanMovieTitle}” 解析失败, statusCode：${statusCode} `, resData.data);
@@ -153,7 +168,7 @@ async function getDoubanList(pageIndex: number, tagIndex: number, sort: string){
             const resData = await HttpsUtil.getAsync({
                 hostname: 'movie.douban.com',
                 path: `/j/search_subjects?type=movie&tag=${encodeURIComponent(tags[tagIndex])}&sort=${sort}&page_limit=${pageLimit}&page_start=${pageStart}`,
-                headers: radomHeaders
+                headers: radomHeader
             }, 'utf-8');            
             //延时，免得被豆瓣封ip
             await PublicFunction.delay(delayTime);
@@ -162,6 +177,10 @@ async function getDoubanList(pageIndex: number, tagIndex: number, sort: string){
                 httpsSuccessCount++;
                 const {subjects: tempList} = JSON.parse(resData.data);
                 subjects = tempList;
+                //20次请求换一次header
+                if(httpsSuccessCount % 20 === 0){
+                    radomHeader = getRadomHeaders();
+                }
                 break;
             }else{
                 console.log(`/***** 请求“${tags[tagIndex]}”的列表数据 ${pageStart + 1} 到 ${pageStart + pageLimit} 条出错，statusCode：${statusCode} *****/`);
@@ -169,7 +188,7 @@ async function getDoubanList(pageIndex: number, tagIndex: number, sort: string){
                 if(tryStartIndex === maxTryTimes){
                     return Promise.reject(new Error(`尝试了${maxTryTimes}次，仍然失败`));
                 }
-                radomHeaders = getRadomHeaders();                
+                radomHeader = getRadomHeaders();                
             }
         } catch (error) {
             return Promise.reject(error);
@@ -194,11 +213,12 @@ function getRadomCookie() {
  * 获取随机的headers
  */
 function getRadomHeaders(){
-    const radomIndex = Math.floor(Math.random() * doubanCookies.length );
-    console.log(`radomIndex is ${radomIndex}`);
+    const radomCookieIndex = Math.floor(Math.random() * doubanCookies.length );
+    const radomUserAgentIndex = Math.floor(Math.random() * doubanCookies.length );
+    console.log(`radomCookieIndex is ${radomCookieIndex}, radomUserAgentIndex is ${radomUserAgentIndex}`);
     const headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-        'Cookie': doubanCookies[radomIndex]
+        'User-Agent': userAgents[radomUserAgentIndex],
+        'Cookie': doubanCookies[5]
     }
     return headers;
 }
