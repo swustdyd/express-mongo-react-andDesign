dfe54060b0afb869af5787824d57210866bc719c
aarondeng <1562044678@qq.com>
2018-06-07 14:04:24 +0800

    将数据库的生产独立出来

---
+++ server/src/db/sync.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+import logger from '../common/logger'
+import {sequelize, modelSyncOptions} from './sequelize'
+
+fs.readdirSync(path.resolve(__dirname, '../models')).filter((file) => {
+    return (file.indexOf('.') !== 0) && (file.slice(-3) === '.js');
+}).forEach((file) => {
+    const model = require(path.resolve(__dirname, `../models/${file}`));
+    sequelize.models[model.name] = model;
+})
+
+//执行更新数据库结构操作
+
+const isDev = process.env.NODE_ENV !== 'production';
+isDev && sequelize.sync({
+    // force: true 如果表已经存在，将会丢弃表
+    force: false,
+    // Alters tables to fit models. Not recommended for production use. 
+    // Deletes data in columns that were removed or had their type changed in the model.
+    alter: true
+}).then(() => {
+    console.log('db buidl success');
+}).catch((err) => {
+    console/log('db build failed');
+    logger.log(err);
+});
