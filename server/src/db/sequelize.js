8311d8287f84f0e86234495d57cc026098de118c
aarondeng <1562044678@qq.com>
2018-06-07 17:29:42 +0800

    连接池改为100

--- server/src/db/sequelize.js
+++ server/src/db/sequelize.js
@@ -4,7 +4,7 @@ export const sequelize = new Sequelize('test', 'root', 'DD89757000', {
     host: 'localhost',
     dialect: 'mysql',
     pool: {
-        max: 10,
+        max: 100,
         min: 0,
         acquire: 30000,
         idle: 10000
