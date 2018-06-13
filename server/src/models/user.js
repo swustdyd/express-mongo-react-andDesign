408ade2c9d930c9bab862f74fbfa8263ae637a33
aarondeng <1562044678@qq.com>
2018-06-13 14:02:43 +0800

    添加了user表

---
+++ server/src/models/user.js
@@ -0,0 +1,42 @@
+import logger from '../common/logger'
+import moment from 'moment'
+import {sequelize, DataTypes} from '../db/sequelize'
+import { dateFormatString } from '../../../baseConfig'
+
+const User = sequelize.define('user', {
+    userId:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING(100)
+    },
+    password: {
+        type: DataTypes.STRING(100)
+    },
+    icon: {
+        type: DataTypes.STRING(100)
+    },
+    role: {
+        type: DataTypes.INTEGER
+    },
+    createAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+        get(){
+            return moment(this.getDataValue('createAt')).format(dateFormatString);
+        }
+    },
+    updateAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+        get(){
+            return moment(this.getDataValue('updateAt')).format(dateFormatString);
+        }
+    }
+})
+
+export default User;
