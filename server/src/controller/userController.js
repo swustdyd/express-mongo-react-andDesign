bdcd785a855d0c0995673a71e4313177186db03b
aarondeng <1562044678@qq.com>
2018-06-13 17:28:11 +0800

    根据jwt，作相应的修改

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -1,13 +1,10 @@
-/*
- * @Author: yedong.deng 
- * @Date: 2018-05-10 17:24:27 
- * @Last Modified by:   aaron.deng 
- * @Last Modified time: 2018-05-10 17:24:27 
- */
+import jwt from 'jsonwebtoken'
 import UserService from '../service/user'
 import PubFunction from '../common/publicFunc'
 import BusinessException from '../common/businessException'
 import BaseController from './baseController';
+import {tokenSecret} from '../../../baseConfig'
+import Condition from '../db/condition'
 
 export default class UserController extends BaseController{
     constructor(){
@@ -24,20 +21,25 @@ export default class UserController extends BaseController{
     async signup(request, response, next){
         try{
             const {user: _user} = request.body;
-            let resData = await this.userService.getUsersByCondition({
-                condition: {
-                    name: _user.name
-                }
-            });
-            if(resData.result && resData.result.length > 0){
+            const condition = new Condition();
+            condition.addWhere({
+                name: 'name',
+                value: _user.name
+            })
+            const pageResult = await this.userService.getUsersByCondition(condition);
+            if(pageResult.result && pageResult.result.length > 0){
                 next(new BusinessException('该用户名已存在'));
             }else {
-                resData = await this.userService.saveOrUpdateUser(_user);
-                if(resData.success){
-                    resData.result = '';
-                    resData.message = '注册成功'
+                const newUser = await this.userService.saveOrUpdateUser(_user);
+                const apiResponse = {};
+                if(newUser){
+                    apiResponse.success = true;
+                    apiResponse.message = '注册成功'
+                }else{
+                    apiResponse.success = false;
+                    apiResponse.message = '注册失败'
                 }
-                response.json(resData);
+                response.json(apiResponse);
             }
         }catch (e){
             next(e);
@@ -54,32 +56,34 @@ export default class UserController extends BaseController{
         try{
             const {user: _user, sevenDay} = request.body;
             const {name, password} = _user;
-            const resData = await this.userService.getUsersByCondition({
-                condition:{
-                    name: name
-                }
-            });
-            const users = resData.result;
+            const condition = new Condition();
+            condition.addWhere({
+                name: 'name',
+                value: name
+            })
+            const pageResult = await this.userService.getUsersByCondition(condition);
+            const users = pageResult.result;
             if(!users || users.length < 1) {
                 next(new BusinessException('用户名不存在！'));
             }else{
-                const isMatch = await PubFunction.comparePassword(password, users[0].password);
+                const user = users[0];
+                const isMatch = await PubFunction.comparePassword(password, user.password);
                 if(isMatch){
-                    const user = users[0];
                     //过滤敏感信息
                     user.password = '';
+                    //默认过期时间30分钟
+                    let expiresIn = 30 * 60;
+                    //七天免登录
                     if(sevenDay){
-                        //七天免登录
-                        request.session.cookie.maxAge = 1000 * 60 * 60 * 24 * 7;
-                    }else{
-                        //30分钟
-                        request.session.cookie.maxAge = 1000 * 60 * 30;
+                        expiresIn = 7 * 24 * 60 * 60;
                     }
-                    request.session.user = user;
-                    response.cookie('extra', { a: 1, b: 2, c: 3}, { maxAge: 30 * 60 * 1000});
+                    const token = jwt.sign({user}, tokenSecret, {
+                        expiresIn
+                    })
                     response.json({
                         success: true,
-                        message: '登录成功'
+                        message: '登录成功',
+                        token
                     });
                 }else{
                     next(new BusinessException('用户名或密码错误！'));
@@ -95,14 +99,15 @@ export default class UserController extends BaseController{
      * @param {*} request 
      * @param {*} response 
      */
-    logout(request, response) {
-        request.session.destroy((err) => {
-            if(err){
-                next(err);
-            }else{                
-                response.json({success: true, message: '登出成功'});
-            }
-        })
+    logout(request, response, next) {
+        // request.session.destroy((err) => {
+        //     if(err){
+        //         next(err);
+        //     }else{                
+        //         response.json({success: true, message: '登出成功'});
+        //     }
+        // })
+        next(new BusinessException('登出功能已弃用'));
     }
 
     /**
@@ -113,7 +118,7 @@ export default class UserController extends BaseController{
      */
     async getUsers(request, response, next) {
         try {
-            const {pageIndex, pageSize} = request.query;
+            const {offset, pageSize} = request.query;
             let condition = request.query.condition || '{}';
             condition = JSON.parse(condition);
 
@@ -127,12 +132,8 @@ export default class UserController extends BaseController{
             if(condition._id){
                 newCondition._id = condition._id;
             }
-            const resData = await this.userService.getUsersByCondition({
-                condition: newCondition,
-                pageIndex: pageIndex,
-                pageSize: pageSize
-            });
-            response.json(resData);
+            const pageResult = await this.userService.getUsersByCondition(new Condition());
+            response.json({success: true, ...pageResult});
         }catch (e){
             next(e);
         }
@@ -182,14 +183,15 @@ export default class UserController extends BaseController{
      * @param {*} next 
      */
     checkLogin(request, response, next) {
-        if(request.session.user){
-            response.json({
-                success: true,
-                result: request.session.user
-            })
-        }else {
-            response.json({success: false})
-        }
+        // if(request.session.user){
+        //     response.json({
+        //         success: true,
+        //         result: request.session.user
+        //     })
+        // }else {
+        //     response.json({success: false})
+        // }
+        next(new BusinessException('检查登录功能已弃用'));
     }
 
     /**
