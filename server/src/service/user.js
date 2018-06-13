fe313b10d98be8c77dfc4d7193e5180b88af9272
aarondeng <1562044678@qq.com>
2018-06-13 14:25:23 +0800

    修改了user的service，改为从mysql存储

--- server/src/service/user.js
+++ server/src/service/user.js
@@ -1,19 +1,13 @@
-/**
- * Created by Aaron on 2018/1/19.
- */
-//import User from '../models/user'
-const User = {};
+import UserModel from '../models/user'
 import logger from '../common/logger'
 import _ from 'underscore'
 import PubFunction from '../common/publicFunc'
 import BusinessException from '../common/businessException'
 import { QueryDefaultOptions } from '../common/commonSetting'
-import { ObjectId, SingleReturnType, QueryOptionsType, PageReturnType, UserType } from '../common/type';
+import { PageResult } from '../common/type';
 import BaseService from './baseService';
-
-const queryDefaultOptions = QueryDefaultOptions;
-
-
+import {db} from '../db'
+import Condition from '../db/condition'
 
 /**
  * 用户模块service
@@ -21,115 +15,66 @@ const queryDefaultOptions = QueryDefaultOptions;
 export default class UserService extends BaseService{
     /**
      * 根据用户id 获取用户
-     * @param id 用户id
+     * @param userId 用户id
      */
-    getUserById(id: ObjectId) : Promise<SingleReturnType> {
-        return new Promise(function (resolve, reject) {
-            if(!id){
-                reject(new BusinessException('用户id不能为空'))
+    async getUserById(userId: number) : Promise<UserModel> {
+        if(!userId){
+            reject(new BusinessException('用户id不能为空'))
+        }
+        return await UserModel.findOne({
+            where:{
+                userId: userId
             }
-            User.findOne({_id: id}, function (err, user) {
-                if(err){
-                    reject(err);
-                }
-                if(user.icon && user.icon.src){
-                    user.icon.src = PubFunction.rebuildImgUrl(user.icon.src);
-                }
-                resolve({success: true, result: user});
-            })
-        })
+        });            
     }
 
     /**
      * 根据查询条件查询用户
-     * @param customOptions 查询条件
+     * @param condition 查询条件
      */
-    getUsersByCondition(customOptions: QueryOptionsType) : Promise<PageReturnType> {
-        const options = _.extend({}, queryDefaultOptions, customOptions);
-        return new Promise(function (resolve, reject) {
-            User.count(options.condition, function (err, count) {
-                User.find(options.condition)
-                    .sort(options.sort)
-                    .skip(options.pageIndex * options.pageSize)
-                    .limit(options.pageSize)
-                    .exec((err, users) => {
-                        if(err){
-                            reject(err);
-                        }
-                        users.forEach((user) => {
-                            if(user.icon && user.icon.src){
-                                user.icon.src = PubFunction.rebuildImgUrl(user.icon.src);
-                            }
-                        });
-                        resolve({
-                            success: true,
-                            result: users,
-                            total: count,
-                            pageIndex: options.pageIndex,
-                            pageSize: options.pageSize
-                        });
-                    })
-            });
-        });
+    async getUsersByCondition(condition: Condition) : Promise<PageResult> {
+        const total = await db.count(condition);
+        const result = await db.query(condition.toSql(), {type: db.QueryTypes.SELECT});
+        return {result, total};
     }
 
     /**
      * 保存或者更新用户
-     * @param _user 用户信息
+     * @param user 用户信息
      */
-    async saveOrUpdateUser(_user: UserType) : Promise<SingleReturnType> {
-        let inputUser = _user;
-        if(inputUser.password){
+    async saveOrUpdateUser(user: UserModel) : Promise<SingleReturnType> {
+        if(user.password){
             //加密用户密码
-            const bcryptPassword = await PubFunction.bcryptString(inputUser.password);
-            inputUser.password = bcryptPassword;
+            const bcryptPassword = await PubFunction.bcryptString(user.password);
+            user.password = bcryptPassword;
         }
-        if(inputUser._id){
+        if(user.userId){
             //修改用户
-            const resData = await this.getUserById(inputUser._id);
-            const originUser = resData.result;
-            originUser.meta.updateAt = Date.now();
-            inputUser = _.extend(originUser, inputUser);
+            const originUser = await this.getUserById(user.userId);
+            originUser.updateAt = Date.now();
+            user = _.extend(originUser, user);
+            user = await originUser.update(user)
         }else{
-            //新增用户
-            inputUser = new User(inputUser);
-            inputUser.meta.createAt = inputUser.meta.updateAt = Date.now();
+            user = await UserModel.create(user);
         }
-
-        //保存用户到数据库
-        return new Promise(function(resolve, reject){
-            if(inputUser.icon && inputUser.icon.src){
-                const parseResult = PubFunction.parseUrl(inputUser.icon.src);
-                if(parseResult){
-                    inputUser.icon.src = parseResult.path;
-                }
-            }
-            inputUser.save(function (err, user) {
-                if(err){
-                    logger.error('保存用户时发生错误');
-                    reject(err);
-                }
-                resolve({success: true, result: user});
-            });
-        });
+        return user;
     }
 
 
     /**
      * 根据id删除用户
-     * @param id 用户id
+     * @param userId 用户id
      */
-    deleteUserById(id: ObjectId) : Promise<{success: boolean, message: string}>  {
-        return new Promise(function (resolve, reject) {
-            if(!id){
-                reject(new BusinessException('id不能为空'))
+    async deleteUserById(userId: number) : Promise<boolean>  {
+        if(!userId){
+            throw new BusinessException('用户id不能为空');
+        }
+        const result = await db.query('delete from user where userId = :id', {
+            replacements:{
+                id: parseInt(userId)
             }
-            User.remove({_id: id}, function (err) {
-                if(err){
-                    reject(err);
-                }
-                resolve({success: true, message: '删除成功'});
-            })
-        });
+        })
+        const {affectedRows} = result[1];
+        return affectedRows > 0;
     }
 }
