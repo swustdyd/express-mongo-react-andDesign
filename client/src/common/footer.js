3d261f909a8e850fe0272cc2a14852330a5973cd
aarondeng <1562044678@qq.com>
2018-08-01 09:43:01 +0800

    footer版权动态更新

--- client/src/common/footer.js
+++ client/src/common/footer.js
@@ -2,12 +2,13 @@
  * Created by Aaron on 2018/3/26.
  */
 import React from 'react'
+import moment from 'moment'
 
 class Footer extends React.Component{
     render(){
         return(
             <div>
-                Demo ©2016 Created by AaronDeng
+                Demo ©2016~{moment(new Date()).format('YYYY')} Created by AaronDeng
             </div>
         );
     }
