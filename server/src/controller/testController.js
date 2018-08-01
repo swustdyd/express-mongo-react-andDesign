6bcb6e4325a4c9f22b99e1161ce66e32b87f4771
aarondeng <1562044678@qq.com>
2018-08-01 15:01:01 +0800

    去除不必要的依赖

--- server/src/controller/testController.js
+++ server/src/controller/testController.js
@@ -1,34 +1,11 @@
-/*
- * @Author: dyd 
- * @Date: 2018-05-10 20:38:22 
- * @Last Modified by: dyd
- * @Last Modified time: 2018-05-10 20:43:32
- */
-import $ from 'cheerio'
-import https from 'https'
-import request from 'request-promise-native'
-import HttpsUtil from '../common/httpsUtil'
 import BaseController from './baseController';
-import PublicFunction from '../common/publicFunc'
-import crypto from 'crypto'
-import HttpsProxyAgent from 'https-proxy-agent'
-import Url from 'url'
-import jade from 'jade'
-import EmailUtil from '../common/emailUtil'
-import path, { resolve } from 'path'
-import BaseConfig from '../../../baseConfig'
-import moment from 'moment'      
-import logger from '../common/logger';        
-import MovieService from '../service/movie'
-import Condition, {JoinType, OpType, LogicOpType} from '../db/condition'
-import { db } from '../db';
-import {log, before, after, requestSignin, requestAdmin, requestSuperAdmin, route, Method, controller} from '../common/decorator'
+import { log, before, after, requestSignin, requestAdmin, 
+    requestSuperAdmin, route, Method, controller } from '../common/decorator'
 
 @controller('/test')
 export default class TestController extends BaseController{
     constructor(){
         super();
-        this._movieService = new MovieService();
     }
 
     /**
